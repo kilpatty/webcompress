7680fab85a36aa14331a80b9fa98737a4ecf1a75
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2017-01-02 14:29:32 -0600

    change var names on brotli and zopfli compression

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var program = require('commander');
 program
     .version('0.1.0')
     .arguments('<file>')
-    .option('-b, --brotli <brotliEnabled>', 'False is brotli compression should not be enabled')
-    .option('-z, --zopfli <zopfliEnabled>', 'False is Zopfli compression should not be enabled')
+    .option('-b, --brotli', 'False is brotli compression should not be enabled')
+    .option('-z, --zopfli', 'False is Zopfli compression should not be enabled')
     .action(function(file) {
         // Check if this file is a directory?
         // If it is a directory, then automatically compress the entire directory?
@@ -15,14 +15,16 @@ program
         // If no -r and directory then prompt the user.
 
         //Default Brotli compression to true.
-        if (!program.brotliEnabled) {
-            program.brotliEnabled = true;
+        if (!program.brotli) {
+            program.brotli = true;
         }
+
         //Default zopfli compress to true.
-        if (!program.zopfliEnabled) {
-            program.zopfliEnabled = true;
+        if (!program.zopfli) {
+            program.zopfli = true;
         }
+
      console.log('Brotli Enabled: %s Zopfli Enabled: %s file: %s',
-         program.brotliEnabled, program.zopfliEnabled, file);
+         program.brotli, program.zopfli, file);
   })
   .parse(process.argv);
